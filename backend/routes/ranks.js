bd3b247c9737e2d7846b445845f2d352176b248d
Ashan-Edirisinghe <ashanedirisingheugc@gmail.com>
2025-12-13 23:54:54 +0530

    add: get method
    in rank router

--- backend/routes/ranks.js
+++ backend/routes/ranks.js
@@ -20,7 +20,7 @@ router.post('/add', async (req, res) => {
      });
 
    
-router.push('/count/:movieId', async (req, res) => {
+router.put('/count/:movieId', async (req, res) => {
 
      const movieId = req.params.movieId;
      const update = req.body;
@@ -49,5 +49,27 @@ router.push('/count/:movieId', async (req, res) => {
 
 }
 );   
+
+
+router.get('/:movieTitle', async (req, res) => {
+     
+    const movieTitle = req.params.movieTitle;
+    try{
+
+          const movie = await Rank.findOne({ movieTitle: movieTitle });
+
+          if(!movie){
+            return res.status(404).json({ message: 'Movie not found' });
+          }
+
+          res.status(200).json(movie); 
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+
+});
  
 
+module.exports = router;    
