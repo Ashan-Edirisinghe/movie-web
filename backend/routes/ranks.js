abc016d32db1377327688fa20c0b0ffc9f56aa23
Ashan-Edirisinghe <ashanedirisingheugc@gmail.com>
2025-12-13 23:26:26 +0530

    add: put method
    in routers ranks,js

--- backend/routes/ranks.js
+++ backend/routes/ranks.js
@@ -19,6 +19,35 @@ router.post('/add', async (req, res) => {
         console.log(err);}
      });
 
-     
+   
+router.push('/count/:movieId', async (req, res) => {
+
+     const movieId = req.params.movieId;
+     const update = req.body;
+
+     try{
+
+       const updated = await Rank.findOneAndUpdate(
+        movieId,
+        update,
+        {
+            new: true
+        }
+        );
+
+        if(!updated){
+            return res.status(404).json({ message: 'count not found' });
+        }
+
+        res.status(200).json(updated);
+ 
+     }catch(err){
+        console.log(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+
+
+}
+);   
  
 
