329afc792ef482c990821ce4d7dbef85ca60101e
Ashan-Edirisinghe <ashanedirisingheugc@gmail.com>
2025-12-15 15:28:46 +0530

    edit: models: ranks

--- backend/models/rank.js
+++ backend/models/rank.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const rankSchema = new mongoose.Schema({
   movieTitle: { type: String, required: true },
   movieId: { type: String, required: true, unique: true },
-  movieImg: { type: URL },
-  count: { type: Number, required: true }
+  movieImg: { type: String },
+  count: { type: Number, required: true, default: 1 }
   
 }, 
 { timestamps: false }
